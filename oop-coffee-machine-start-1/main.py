204fccad61668f072e9035a5f98d81d7368af6e2
glafish <galfishbain@gmail.com>
2022-01-06 18:34:18 +0200

    working on the coffee machine

--- oop-coffee-machine-start-1/main.py
+++ oop-coffee-machine-start-1/main.py
@@ -3,23 +3,33 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-
-money_machine = MoneyMachine()
-coffee_maker = CoffeeMaker()
+profit = 0
 menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
 
 is_on = True
 
 while is_on:
-    options = menu.get_items()
-    choice = input(f"What would you like? ({options}): ")
+    choice = input("​What would you like? (espresso/latte/cappuccino): ")
     if choice == "off":
         is_on = False
     elif choice == "report":
-        coffee_maker.report()
         money_machine.report()
+        coffee_maker.report()
     else:
-        drink = menu.find_drink(choice)
+        coffee_maker.is_resource_sufficient()
+        money_machine.make_payment()
+        if money_machine.process_coins():
+            coffee_maker.make_coffee()
 
-        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
-            coffee_maker.make_coffee(drink)
+
+
+
+
+
+
+
+
+
