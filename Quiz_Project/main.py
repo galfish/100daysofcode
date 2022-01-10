db2d07646ec434492663547996d9145343a0efd3
glafish <galfishbain@gmail.com>
2022-01-10 15:09:30 +0200

    New files to coffee_machine_project

--- Quiz_Project/main.py
+++ Quiz_Project/main.py
@@ -9,9 +9,15 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-
-
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+
+print("🏆️🏆️you've completed the quiz🏆️🏆️")
+print(f"your final score was: {quiz.score}/{quiz.question_number}")
+print ("👏️👏👏️👏👏️👏👏️👏👏️👏👏️👏")
+
 
 
